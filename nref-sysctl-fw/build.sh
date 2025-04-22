f651fd4bf900457f7f2d457b0344b07bdd10bc01
Lukas F. Hartmann <lukas@mntre.com>
2025-04-22 19:32:10 +0200

    sysctl: build with sdk from OS

--- nref-sysctl-fw/build.sh
+++ nref-sysctl-fw/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PICO_SDK_PATH=$PWD/../pico-sdk
-export PICO_EXTRAS_PATH=$PWD/../pico-extras
+export PICO_SDK_PATH=/usr/src/pico-sdk
+export PICO_EXTRAS_FETCH_FROM_GIT=1
 
 mkdir -p build
 cd build
